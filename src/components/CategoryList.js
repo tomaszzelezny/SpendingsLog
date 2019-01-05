623bd91226efb188bca00e04f7233466aaa63896
FP\tzelezny <tzelezny@future-processing.com>
2019-01-05 12:26:03 +0100

    add missing Category component

---
+++ src/components/CategoryList.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+class CategoryList extends React.Component
+{
+    render (){
+        return ( <div>" this is the CategoryList Component"</div> );
+    }
+}
+
+export default CategoryList
