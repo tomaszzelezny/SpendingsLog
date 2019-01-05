fa8416511b5b0bcf123fd355589824d4c12e8398
FP\tzelezny <tzelezny@future-processing.com>
2019-01-05 12:22:40 +0100

    add first dummy compoment

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
+    return (   
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
